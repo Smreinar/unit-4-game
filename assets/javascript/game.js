c70e40453f6967c02662cfd448a8e5d07105ee09
smreinar <stevenreinart123@gmail.com>
2020-02-26 13:10:03 -0700

    htmlStructure

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+//number appears at start of game: random number between 19-120
+
+//when crystal is clicked add number to total score on screen
+
+//each crystal has a random hidden value between 1-12
+
+//player wins if score matches the random number at start
+
+//keep track of wins/losses
+
+// after round restarts new number appears and new crystal values appear and score counter reset to 0
