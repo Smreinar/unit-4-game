49e2ebe98df76964f0411259352730f4366f927c
smreinar <stevenreinart123@gmail.com>
2020-02-28 21:26:33 -0700

    jQuery

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# unit-4-game
+The Crystal Game
+In this game you will be testing your memory and math skill at the same time!
+You will be presented with a random number and with that number you must match it with Your Number.
+To match the number you will get four crystals, each one of these crystals will have a different number value. With that you will need to do some math and add your crystals together to match the number.
+Have Fun and Enjoy!
+link: https://smreinar.github.io/unit-4-game/
+
+created by: Steven Reinart
+Email: stevenreinart123@gmail.com
+
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,113 @@
+
+
+
+// on ready
+$(document).ready(function(){
+    //Random Number Generated
+    var generatedNumber = Math.floor(Math.random() * 101 + 19)
+    $('#matchNumber').text(generatedNumber);
+
+    //varibles
+    var randomNumber = [];
+    var scored = 0;
+    var win = 0;
+    var loss = 0;
+    $(".wins").text(win);
+    $(".losses").text(loss);
+    
+    //functions 
+    function crystalNumber() {
+        for ( var i = 0; i < 4; i++){
+            var number = Math.floor(Math.random() * 11 + 1);
+            randomNumber.push(number);
+        }
+    }
+    crystalNumber();
+
+    //rest function
+    function resetGame() {
+        generatedNumber = Math.floor(Math.random()* 101 + 19);
+        $('#matchNumber').text(generatedNumber);
+        randomNumber = [];
+        crystalNumber();
+        scored = 0;
+        $('#score').text(scored);
+    }
+
+    //win function
+    function winner() {
+        alert("You Won That Round!");
+        win++;
+        $(".wins").text(win);
+        resetGame();
+    }
+
+    //lost function
+    function lost() {
+        alert("You Lost That Round!");
+        loss++;
+        $(".losses").text(loss);
+        resetGame();
+    }
+
+    //On Click Green Crystal
+    $(".greenCrystal").on('click', function() {
+        scored = scored + randomNumber[0];
+        $('#score').text(scored);
+
+        if (scored == generatedNumber) {
+            winner();
+        }
+        else if (scored > generatedNumber) {
+            lost();
+        }
+    })
+
+    //On Click Pink Crystal
+    $(".pinkCrystal").on('click', function() {
+        scored = scored + randomNumber[1];
+        $('#score').text(scored);
+
+        if (scored == generatedNumber) {
+            winner();
+        }
+        else if (scored > generatedNumber) {
+            lost();
+        }
+    })
+
+    //On Click Red Crystal
+    $(".redCrystal").on('click', function() {
+        scored = scored + randomNumber[2];
+        $('#score').text(scored);
+
+        if (scored == generatedNumber) {
+            winner();
+        }
+        else if (scored > generatedNumber) {
+            lost();
+        }
+    })
+
+    //On Click Blue Crystal
+    $(".blueCrystal").on('click', function() {
+        scored = scored + randomNumber[3];
+        $('#score').text(scored);
+
+        if (scored == generatedNumber) {
+            winner();
+        }
+        else if (scored > generatedNumber) {
+            lost();
+        }
+    })
+})
+
+
+
+
+//Breaking It Down:
+
 //number appears at start of game: random number between 19-120
 
 //when crystal is clicked add number to total score on screen
@@ -8,4 +118,7 @@
 
 //keep track of wins/losses
 
-// after round restarts new number appears and new crystal values appear and score counter reset to 0
+// after round resets: new number appears and new crystal numbers appear and scored counter reset to 0
+
+
+
